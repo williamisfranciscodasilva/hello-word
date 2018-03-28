1612ca139832c1597ae6c452f3b9bbef26f49a5c
Williamis Silva <williamisepaula@gmail.com>
2018-03-28 10:26:24 -0300

    declarando variaveis no meu-arquivo.js

--- meu-arquivo.js
+++ meu-arquivo.js
@@ -1,1 +1,17 @@
 alert('hello word');
+var nome = prompt('Digite seu nome: ');
+alert(nome + ', seja bem vindo!');
+  /* Este é um script para cálculo de idade! */
+ 
+  // Declara o ano atual para fazer o cálculo
+  var anoAtual = 2018;
+  
+   // Pede que o usuário digite o ano em que nasceu
+   var anoNascimento = prompt('Digite o ano em que você nasceu.');
+  
+   // Calcula a idade do usuário e armazena na variável idade
+   var idade = anoAtual - anoNascimento;
+  
+   // Mostra ao usuário a idade que ele possui
+   alert("Sua idade é: " + idade + " anos");
+ 
